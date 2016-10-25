ff224e7ab0b199be333913fded15a22c0727b003
lynzz <lynzz168@gmail.com>
2016-10-25 10:07:26 +0800

    6to5 改为 babel,并修复打包 es6 代码压缩出错问题

---
+++ test/fixtures/entry-without-pkg/build.js
@@ -0,0 +1,1 @@
+define("/a",[],function(n,o,e){console.log("a.js")});
--- test/fixtures/to5/a.js
+++ test/fixtures/to5/a.js
@@ -1,1 +1,44 @@
 var [a, , b] = [1,2,3];
+var index = 1
+var c = `${index}`
+const state = (index) => {
+  console.log(index)
+}
+const test = {
+  index: state => state.index,
+  total: state => state.total
+}
+const mutations = {
+  addTodo (state, { text }) {
+    state.todos.push({
+      text,
+      done: false
+    })
+  },
+
+  deleteTodo (state, { todo }) {
+    state.todos.splice(state.todos.indexOf(todo), 1)
+  },
+
+  toggleTodo (state, { todo }) {
+    todo.done = !todo.done
+  },
+
+  editTodo (state, { todo, value }) {
+    todo.text = value
+  },
+
+  toggleAll (state, { done }) {
+    state.todos.forEach((todo) => {
+      todo.done = done
+    })
+  },
+
+  clearCompleted (state) {
+    state.todos = state.todos.filter(todo => !todo.done)
+  }
+}
+module.exports = {
+  state,
+  mutations
+}
